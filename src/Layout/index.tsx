96ea3633e804630cce12e0c2aae2432576355b8f
Carlos-860 <carlostimotheus5@gmail.com>
2024-05-07 17:47:57 +0200

    Movies App Search fucntionality

---
+++ src/Layout/index.tsx
@@ -0,0 +1,35 @@
+import { Box } from "@mui/material";
+import { ReactNode } from "react";
+import Sidebar from "../components/sidebar";
+
+interface LayoutProps {
+    children: ReactNode,
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <div>
+            <Box sx={{
+                backgroundColor: "#10141F",
+                display: "flex",
+                flexDirection: {
+                    xs: "column",
+                    lg: "row"
+                },
+                color: "white",
+                padding: 3,
+                gap: 3,
+                overflowY: "hidden",
+                height: "100vh",
+            }}>
+                <Sidebar />
+                <Box sx={{
+                    width: "100%",
+                    overflowY: "scroll"
+                }}>{children}</Box>
+            </Box>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,9 @@
+import HomeIcon from './icon-nav-home.svg'
+import MovieIcon from './icon-nav-movies.svg'
+import TvSeriesIcon from './icon-nav-tv-series.svg'
+import BookmarkIcon from './icon-nav-bookmark.svg'
+import SearchIcon from './icon-search.svg'
+import moviesIcon from './icon-category-movie.svg'
+import tvSeriesIcon from './icon-category-tv.svg'
+
+export { HomeIcon, MovieIcon, TvSeriesIcon, BookmarkIcon, SearchIcon, moviesIcon, tvSeriesIcon }
